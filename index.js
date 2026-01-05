9a2989fac675ca10b50c679fd4e0c8c4a2ee900a
ananda17gb <ananda.widigdo17@gmail.com>
2026-01-06 00:49:59 +0700

    fixing index.js

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import express, { json } from "express";
 import mongoose, { connect } from "mongoose";
 import cors from "cors";
-import passport from "passport";
-import "./configs/passport.js";
-import http from "http";
+import passport from "./configs/passport.js";
 import authRoutes from "./routes/auth.js";
 import chatRoutes from "./routes/chat.js";
 import petsRoutes from "./routes/pets.js";
 import postsRoutes from "./routes/posts.js";
-import fakeDoorRoutes from "./routes/fakeDoor.js";
 
-dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
-const allowedOrigins = [
-  "https://pat-a-pet-web-git-fakedoor-ananda-arti-widigdos-projects.vercel.app",
-  "http://localhost:5173", // for local dev
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-
 app.use(json());
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    // origin: ["http://10.0.2.2:5000/api", "http://localhost:3000/api"],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(passport.initialize());
-
-// const server = http.createServer(app);
 
 connect(process.env.MONGO_URI)
   .then(() =>
@@ -48,7 +29,7 @@ connect(process.env.MONGO_URI)
   )
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
@@ -60,6 +41,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/pets", petsRoutes);
 app.use("/api/posts", postsRoutes);
-app.use("/api/fake-door", fakeDoorRoutes);
+app.use("/api/fake-door", postsRoutes);
 
 export default app;
