44f626bd8e1b6f97093a2925b232623189a5bb0b
ananda17gb <ananda.widigdo17@gmail.com>
2026-01-06 01:43:32 +0700

    fixing hosting

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-import dotenv from "dotenv";
-import express from "express";
-import serverless from "serverless-http";
-import connectDB from "./configs/db.js";
-import app from "./configs/express.js";
-import routes from "./routes/index.js";
+import "dotenv/config";
+import express, { json } from "express";
+import mongoose, { connect } from "mongoose";
+import cors from "cors";
+import passport from "passport";
+import "./configs/passport.js";
+import http from "http";
+import authRoutes from "./routes/auth.js";
+import chatRoutes from "./routes/chat.js";
+import petsRoutes from "./routes/pets.js";
+import postsRoutes from "./routes/posts.js";
 
-// Connect DB once (handled per cold start)
-let dbReady = false;
-const ensureDB = async () => {
-  if (!dbReady) {
-    await connectDB();
-    dbReady = true;
-  }
-};
+const PORT = process.env.PORT;
+const app = express();
 
-// Attach routes
-app.use("/api", routes);
+app.use(json());
+app.use(
+  cors({
+    // origin: ["http://10.0.2.2:5000/api", "http://localhost:3000/api"],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-// Export handler for Vercel
-export const handler = serverless(app);
+app.use(passport.initialize());
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5001;
-  app.listen(PORT, () => console.log(`Running on ${PORT}`));
-}
+const server = http.createServer(app);
+
+connect(process.env.MONGO_URI)
+  .then(() =>
+    console.log("MongoDB Connected on database:", mongoose.connection.name),
+  )
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello frontend, here's backend");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/pets", petsRoutes);
+app.use("/api/posts", postsRoutes);
